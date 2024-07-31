31671922db0c72fd99da55ecb0bbc90e1068dad0
aalokyadav7 <156888490+aalokyadav7@users.noreply.github.com>
2024-07-31 17:02:44 +0000

    datatypes

--- Basics/dataTypes.js
+++ Basics/dataTypes.js
@@ -39,3 +39,20 @@ console.log("Hello World...!");
 //  know datatype
 // console.log(typeof bigNumber);
 console.log(typeof myFun);
+
+/*
+Return type of variables in JavaScript
+1) Primitive Datatypes
+       Number => number
+       String  => string
+       Boolean  => boolean
+       null  => object
+       undefined  =>  undefined
+       Symbol  =>  symbol
+       BigInt  =>  bigint
+
+2) Non-primitive Datatypes
+       Arrays  =>  object
+       Function  =>  function
+       Object  =>  object
+*/
