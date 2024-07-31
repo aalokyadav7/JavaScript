9349b137696b7f9d6b26256c4f45ab5b8162b36b
aalokyadav7 <156888490+aalokyadav7@users.noreply.github.com>
2024-07-31 12:08:21 +0000

    operation

--- Basics/converstion.js
+++ Basics/converstion.js
@@ -7,3 +7,7 @@ console.log(typeof(score));
 let valueNumber = Number(score);
 
 console.log(valueNumber);
+
+/*"33" =>33
+"33adf" => NaN
+true => 1; false => 0   */
---
+++ Basics/operation.js
@@ -0,0 +1,23 @@
+let str1 = "Hello"
+let str2 = "World"
+let str3 = str1 + str2
+
+
+// console.log(str3);
+// console.log(+true);
+// console.log(+"");
+
+// assingment oprator
+let num1 , num2, num3 
+num1= num2= num3= 2 + 2
+let gamecounter = 200
+// gamecounter++;
+++gamecounter;
+console.log(gamecounter);
+
+
+/*
+If used postfix, with operator after operand (for example, x++), the increment operator increments and returns the value before incrementing.
+
+// If used prefix, with operator before operand (for example, ++x), the increment operator increments and returns the value after incrementing.
+*/
