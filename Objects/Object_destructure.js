c2df4667f0ea9118f8936020dcc2a4d9f575ca18
aalokyadav7 <156888490+aalokyadav7@users.noreply.github.com>
2024-08-03 12:09:51 +0000

    Object destru..

---
+++ Objects/Object_destructure.js
@@ -0,0 +1,30 @@
+const course = {
+    courseName : "Js in Hindi",
+    price : "1000",
+    courseInstructure : "ABcD"
+}
+const {courseInstructure :Instructure} = course
+
+console.log(Instructure);
+
+// React Consept
+// const navBar = ({company}) => {
+
+// }
+// navBar(company = "abcd")
+
+/*
+{
+    "name" : "ABCD",
+    "courseName" : "JavaScript",
+    "price" : "Free"
+},
+
+Api in Json formate
+[
+{}
+{}
+{}
+]
+
+*/
